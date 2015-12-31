cce9f65187ceb21d0fbfc6d47fddbe37700ebbdd
Len <Len-PGH@users.noreply.github.com>
2015-12-31 06:05:48 -0500

    Update install_fusionpbx.bat

--- install/windows/install_fusionpbx.bat
+++ install/windows/install_fusionpbx.bat
@@ -1,10 +1,10 @@
-@ECHO OFF
+@ ECHO OFF
 ECHO Use This Install AT YOUR OWN RISK!!!
-Echo this will install and configure FusionPBX, FreeSWITCH, PostgreSQL, PHP and NGINX.
+Echo This will install and configure FusionPBX, FreeSWITCH, PostgreSQL, PHP and NGINX.
     
-    @echo on
+    REM //@echo on
      
-    title Install FusionPBX on Windows
+    	title Install FusionPBX on Windows
 	ECHO Let's Install!
 	ECHO Please wait just a few moments.....
 	
@@ -13,16 +13,13 @@ Echo this will install and configure FusionPBX, FreeSWITCH, PostgreSQL, PHP and 
 	Pause Going to download Freeswitch
 	powershell -Command "(New-Object Net.WebClient).DownloadFile('http://files.freeswitch.org/windows/installer/x64/freeswitch.msi', 'freeswitch.msi')"
 	powershell -Command "Invoke-WebRequest http://files.freeswitch.org/windows/installer/x64/freeswitch.msi -OutFile freeswitch.msi"
-	REM // Powershell.exe -executionpolicy remotesigned $client = new-object System.Net.WebClient
-	REM // Powershell.exe -executionpolicy remotesigned $client.DownloadFile("http://files.freeswitch.org/windows/installer/x64/freeswitch.msi", "%Downloads%")
-	REM // starting install of freeswitch
-  REM // cd "%Downloads%"
+	
 	ECHO Going to install Freeswitch
 	Pause
-  start /wait freeswitch.msi /quiet
+  	start /wait freeswitch.msi /quiet
 	Pause Going to download Postgresql
 	
-  powershell -Command "(New-Object Net.WebClient).DownloadFile('http://get.enterprisedb.com/postgresql/postgresql-9.4.5-3-windows-x64.exe', 'postgresql-9.4.5-3-windows-x64.exe')"
+  	powershell -Command "(New-Object Net.WebClient).DownloadFile('http://get.enterprisedb.com/postgresql/postgresql-9.4.5-3-windows-x64.exe', 'postgresql-9.4.5-3-windows-x64.exe')"
 	powershell -Command "Invoke-WebRequest http://get.enterprisedb.com/postgresql/postgresql-9.4.5-3-windows-x64.exe -OutFile postgresql-9.4.5-3-windows-x64.exe"
 	Pause
 	ECHO Going to install Postgresql
@@ -47,19 +44,37 @@ Echo this will install and configure FusionPBX, FreeSWITCH, PostgreSQL, PHP and 
 	unzip php-7.0.1-nts-Win32-VC14-x64.zip
 	
 	pause
-	echo Download FusionPBX
-	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/fusionpbx/fusionpbx/archive/4.0.0.zip', '4.0.0.zip')"
-	powershell -Command "Invoke-WebRequest https://github.com/fusionpbx/fusionpbx/archive/4.0.0.zip -OutFile 4.0.0.zip"
-	
 	cd "C:/nginx-1.9.9/html"
-	unzip 4.0.0.zip
-	del 4.0.0.zip
+	echo Download FusionPBX
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/fusionpbx/fusionpbx/archive/master.zip', 'master.zip')"
+	powershell -Command "Invoke-WebRequest https://github.com/fusionpbx/fusionpbx/archive/master.zip -OutFile master.zip"
+	
+	unzip master.zip
+	del master.zip
 	echo goto http://localhost to do web gui install part. Come back and press enter to continue after that.
 	pause
 	
+	echo Download Git
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.6.4.windows.1/Git-2.6.4-64-bit.exe', 'Git-2.6.4-64-bit.exe')"
+	powershell -Command "Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.6.4.windows.1/Git-2.6.4-64-bit.exe -OutFile Git-2.6.4-64-bit.exe"
+	echo install git
+	pause 
+	start /wait Git-2.6.4-64-bit.exe /quiet
+	del Git-2.6.4-64-bit.exe
+	pause
+	cd "C:/nginx-1.9.9"
+	echo Download Memcached-1.4.4-14
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('http://downloads.northscale.com/memcached-win64-1.4.4-14.zip', 'memcached-win64-1.4.4-14.zip')"
+	powershell -Command "Invoke-WebRequest http://downloads.northscale.com/memcached-win64-1.4.4-14.zip -OutFile memcached-win64-1.4.4-14.zip"
 	
+	unzip memcached-win64-1.4.4-14.zip
+	del memcached-win64-1.4.4-14.zip
+	memcached.exe -d install
+	pause
+	REM // next part need to configure nginx.conf, php.ini(might have this pre-done and cp it from release download)
 	
+	REM // next part create databases for postgresql
 	
-	
-	
-    
+    	REM // start nginx etc
+    	
+    	REM // goto gui to install
